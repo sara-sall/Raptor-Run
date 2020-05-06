b06bcf9bb7f80901e86b8248452f1b493cd6e525
sara-sall <sarasall092@gmail.com>
2020-05-06 16:23:25 +0200

    Adding obstacles

--- RunnerGame/Assets/Scripts/SpawnObstacles.cs
+++ RunnerGame/Assets/Scripts/SpawnObstacles.cs
@@ -4,15 +4,31 @@ using UnityEngine;
 
 public class SpawnObstacles : MonoBehaviour
 {
+
+    public GameObject[] obstacles;
+    private GameObject obj;
+    private float XPos;
+    private int number;
+
+    private float randomPosition;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        InvokeRepeating("Spawn", 1.5f, 1.6f);
+        //Start method Spawn in 1.5s, repeat every 1.6s
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Spawn()
     {
-        
+        if (!GameObject.Find("Dino").GetComponent<Dino>().isDead)
+        {
+            number = Random.Range(0, 7);
+            randomPosition = Random.Range(21, 31);
+            XPos = transform.position.x + randomPosition;
+            obj = obstacles[number] as GameObject;
+
+            Instantiate(obj, new Vector2(XPos, obj.transform.position.y), Quaternion.identity);
+        }
     }
 }
---
+++ RunnerGame/Assets/Scripts/StartFlying.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StartFlying : MonoBehaviour
+{
+
+    private Animator anim;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+        anim.SetTrigger("start");
+    }
+
+
+}
